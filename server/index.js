67f48f3de74679eeb592423644bec6da4129cb81
sahil937 <meharsahil207@gmail.com>
2022-11-19 01:59:18 -0600

    Update: Initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(port, console.log(`Server running on port ${port}`));
