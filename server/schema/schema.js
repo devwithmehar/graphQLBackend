d88722d4b8811c3ea17d440d2c2bb6416129e614
sahil937 <meharsahil207@gmail.com>
2022-11-19 17:41:49 -0600

    Update: GraphQL Schema is defined as per Mongoose Schemas

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,6 @@
-const { projects, clients } = require('../sampleData');
+// Mongoose Models
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
 const { GraphQLObjectType ,
   GraphQLID,
@@ -29,7 +31,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent,args){
-        return clients.find(client => client.id === parent.clientId );
+        return clients.findById(parent.clientId);
       }
     }
   })
@@ -41,7 +43,7 @@ const RootQuery = new GraphQLObjectType({
     projects: {
         type: new GraphQLList(ProjectType),
         resolve(parent, args) {
-          return projects;
+          return  Project.find();
         }
 
     },
@@ -49,13 +51,13 @@ const RootQuery = new GraphQLObjectType({
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find(project => project.id === args.id );
+        return Project.findById(args.id)
       }
     },
     clients: {
           type: new GraphQLList(ClientType),
           resolve(parent, args) {
-            return clients;
+            return Client.find();
           }
 
     },
@@ -63,7 +65,7 @@ const RootQuery = new GraphQLObjectType({
         type: ClientType,
         args: { id: { type: GraphQLID } },
         resolve(parent, args) {
-          return clients.find(client => client.id === args.id );
+          return Client.findById(args.id);
         }
     }
   }
