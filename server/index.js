47bb0490b9a5467624d43af10e6ca9f3aa26fac5
sahil937 <meharsahil207@gmail.com>
2022-11-19 19:24:28 -0600

    updateProject mutation is added in graphql schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const colors = require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+
+// Connect to Database
+connectDB();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}));
 
 app.listen(port, console.log(`Server running on port ${port}`));
