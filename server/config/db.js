32a3200efa117c490452021f2f5422c5bd9522b7
sahil937 <meharsahil207@gmail.com>
2022-11-19 18:15:07 -0600

    update: Mongodb is configured

--- server/config/db.js
+++ server/config/db.js
@@ -6,14 +6,16 @@
 
 const connectDB = async () => {
     try{
-      mongoose.connect(
+    const conn = await  mongoose.connect(
         process.env.MONGO_URI,
         { useNewUrlParser: true, useUnifiedTopology: true },
-        () => console.log(`MongoDB is connected`.cyan.underline.bold),
+
       );
+
+      console.log(`MongoDB is connected ${conn.connection.host}`.cyan.underline.bold);
     }
     catch(e){
-      console.log("DB is not connected");
+      console.log("DB is not connected" +  e);
     }
 };
 
